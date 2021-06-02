dd8c85260443e709141bf8edf5672ce85b33b199
Manh Cuong <aoevietnam.org@gmail.com>
2021-06-02 13:27:12 +0700

    add install device service

---
+++ app/services/installed_device_service.rb
@@ -0,0 +1,11 @@
+class InstallDeviceService
+  class << self
+    def get_installed_device(request)
+      counts = User.where('created_at >= ? AND created_at <= ?', start_time.beginning_of_day, end_time.beginning_of_day).group("DATE_TRUNC('day', created_at)")
+                   .order("DATE_TRUNC('day', created_at) DESC").count()
+      counting_report = CountingReport.new
+      counts.each {|date, count| counting_report.add_in_day_count(date, count)}
+      counting_report
+    end
+  end
+end
